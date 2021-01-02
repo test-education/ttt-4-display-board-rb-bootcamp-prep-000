faf447e4fa84f4139627ccf2be5deb68d64557b4
Oleh Yashtulov <signal.oleg@gmail.com>
2021-01-02 23:43:10 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,9 +3,9 @@
 def display_board(board)
 
 puts " #{board[0]} | #{board[1]} | #{board[2]} "
-11.times {print '-'}
+11.times {print "-"}
 puts " #{board[3]} | #{board[4]} | #{board[5]} "
-11.times {print '-'}
+11.times {print "-"}
 puts " #{board[6]} | #{board[7]} | #{board[8]} "
 
 end
